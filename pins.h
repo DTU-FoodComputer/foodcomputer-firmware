a5b76c7e7278deb7318a1635a1635acb863f8409
Thomas Sørensen <thots@cphnano.com>
2019-09-11 11:32:34 +0200

    updated pins.h

--- pins.h
+++ pins.h
@@ -1,7 +1,7 @@
 // Put all the pin definitions here
 #define WATER_MIN_PIN       3
 #define WATER_MAX_PIN       5
-#define BLUE_LED_PIN       40  // K4
+#define BLUE_LED_PIN       13  // TODO set to 40, K4
 #define RED_LED_PIN        41  // K3
 #define WHITE_LED_PIN      42  // K2
 #define NUTRIENT_A_PIN     28 
